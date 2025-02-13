21627ef2ac6a36271599273e8780d4bb4ef205c7
StepanBURNdera <kinich2001@gmail.com>
2025-02-13 15:53:40 +0100

    media server scripts [2]

---
+++ media_server/scripts/media_service.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install apt-transport-https
+
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex-archive-keyring.gpg >/dev/null
+
+echo deb [signed-by=/usr/share/keyrings/plex-archive-keyring.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+
+sudo apt install plexmediaserver
